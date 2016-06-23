351690d6f238a9006460d18dda5de9d318bdedd1
arekenny3 <rkkenny3@siu.edu>
2016-06-23 12:23:11 -0500

    Update login.cs

--- login.cs
+++ login.cs
@@ -1,3 +1,4 @@
+	
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -8,37 +9,81 @@ using System.Text.RegularExpressions;
 
 namespace OilTestController
 {
-
-    //static
-    class Program
+	class Program 
     {
-        public static void Main(string[] args)
-        {
-            
-            /********************Name*******************/
-            bool nameValid = false;
-            do
-            {
-                int n = 25;
-                Console.WriteLine("Name: ");
-                string inputName = Console.ReadLine();
-                nameValid = check_name(n, inputName);
-            } while (!nameValid);
-            Console.WriteLine("Success! ");
-        /*************************Clock Card**********/
-        bool cardValid = false;
-        do
-	{
-        int n = 5; // length of string
-                Console.WriteLine("Clock Card: ");
-                string inputCard = Console.ReadLine();
-                cardValid = check_card(n, inputCard);
-            } while (!cardValid);
-            Console.WriteLine("Success! ");
-        }
-	        
 
-        public static bool check_ParamLength(int n, string input)
+		
+		public static void Main(string[] args)
+		{
+            string Name = "";
+			Name = name(Name);
+            string newName = "";
+            Console.WriteLine("{0}",Name);
+
+            Console.WriteLine("Do you want to edit anything 1: Yes, 2: No ");
+            int ans = Convert.ToInt32(Console.ReadLine());
+            if (ans != 1)
+            {
+                Console.WriteLine("What would you like to edit? ");
+                int ans2 = Convert.ToInt32(Console.ReadLine());
+                if (ans2 == 1)
+                {
+                   newName = name(newName);
+                   Name = newName;
+                   Console.WriteLine("{0}", newName);
+                }
+            }
+            Console.WriteLine("New Name: ", newName);
+
+           // string Card = card();
+
+
+		}
+		
+		public static string name(string Name)
+		{
+			
+			bool nameValid = false;
+            string inputName = "";
+           
+			 do
+                {
+                    int n = 25;
+                    Console.WriteLine("Name: ");
+                    inputName = Console.ReadLine();
+                    nameValid = check_name(inputName, n);
+                } while (!nameValid);
+             if (nameValid)
+             {
+                 Name = inputName;
+             }
+                Console.WriteLine("Success! ");
+                return inputName;
+		}
+		  public static bool check_name(string input, int n)
+        {
+            bool validLength = check_ParamLength(input, n);
+            bool validString = check_ParamLet(input, n);
+            bool validSymb = check_ParamSymb(input, n);
+
+            if ((!validLength) || (!validString) || (!validSymb))
+            {
+                Console.WriteLine("This Test Doesnt work!!!");
+               // isFunky(validLength, validString, validSymb); // Error Analysis
+                return false;
+            }
+            return true;
+        }
+
+        //Card
+          public static string card()
+          {
+
+              return card();
+          }
+
+	 // Checking String Length
+        public static bool check_ParamLength(string input, int n)
         {
             //checking length of user input
             char[] array = input.ToCharArray(); //parsing string into an array to check each ind character
@@ -48,8 +93,8 @@ namespace OilTestController
             }
             return true;
         }
-        // Check if input is all letters
-        public static bool check_ParamLet(int n, string input)
+
+        public static bool check_ParamLet(string input, int n)
         {
             char[] array = input.ToCharArray();
             for (int i = 0; i < array.Length; i++)
@@ -64,7 +109,7 @@ namespace OilTestController
             return true;
         }
         // check if input is all numbers
-        public static bool check_ParamNum(int n, string input)
+        public static bool check_ParamNum(string input, int n)
         {
             char[] array = input.ToCharArray();
             for (int i = 0; i < array.Length; i++)
@@ -79,7 +124,7 @@ namespace OilTestController
             return true;
         }
         // check if input doesn't contain any special characters...(/,!,~,etc...)
-        public static bool check_ParamSymb(int n, string input)
+        public static bool check_ParamSymb(string input, int n)
         {
             bool validSymb;
             Regex RgxUrl = new Regex("[^a-zA-Z0-9]");
@@ -91,57 +136,7 @@ namespace OilTestController
 
             return true;
         }
-      
-        // Error Analysis!!!
-        public static void isFunky(bool validLength, bool validString, bool validSymb)
-        {
-            Console.WriteLine("Problems with input: ");
-            if (!validLength)
-            {
-                Console.WriteLine("Length! ");
-            }
-            if (!validString)
-            {
-                Console.WriteLine("Cannot have Numbers! ");
-            }
-            if (!validSymb)
-            {
-                Console.WriteLine("Cannot contain any special Characters!!! ('*!@,./ ')");
-            }
-        }
-        // check name
-        public static bool check_name(int n, string inputName)
-        {
-            bool validLength = check_ParamLength(n, inputName);
-            bool validString = check_ParamLet(n, inputName);
-            bool validSymb = check_ParamSymb(n, inputName);
+       
 
-            if ((!validLength) || (!validString) || (!validSymb))
-            {
-                Console.WriteLine("This Test Doesnt work!!!");
-                isFunky(validLength, validString, validSymb);
-                return false;
-            }
-            return true;
-        }
-        //check Clockcard
-
-        public static bool check_card(int n, string inputCard)
-        {
-            bool validLength = check_ParamLength(n, inputCard);
-            bool validString = check_ParamNum(n, inputCard); //different than name
-            bool validSymb = check_ParamSymb(n, inputCard);
-
-            if ((!validLength) || (!validString) || (!validSymb))
-            {
-                Console.WriteLine("This Test Doesnt work!!!");
-                isFunky(validLength, validString, validSymb);
-                return false;
-            }
-            return true;
-        }
-
-    }
-
-
+	}	
 }
